7e9f3f2d250fc2d306619f2be8de53c397680545
Lưu Minh Hoàng <ceriagame@gmail.com>
2019-08-17 20:08:46 +0700

    8_157 Calculating the Ingredient Sum Dynamically

--- src/containers/BurgerBuilder/Burger/Burger.js
+++ src/containers/BurgerBuilder/Burger/Burger.js
@@ -5,13 +5,16 @@ import styles from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-  console.log(props.ingredient);
-  const transformedIngredients = Object.keys(props.ingredient)
+  let transformedIngredients = Object.keys(props.ingredient)
     .map(igKey => {
       return [...Array(props.ingredient[igKey])].map((_, i) => {
         return <BurgerIngredient key={igKey + i} type={igKey}/>;
       });
-    });
+    })
+    .reduce((arr, el) => {return arr.concat(el)}, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = (<p>Please start adding ingredient</p>);
+  }
   return (
     <div className={styles.Burger}>
       <BurgerIngredient type='bread-top' />
