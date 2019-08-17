4e727b9759be74f6494c7f2b4dff8edda8cba96f
Lưu Minh Hoàng <ceriagame@gmail.com>
2019-08-17 13:58:22 +0700

    8_151

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Layout from './components/Layout/Layout';
+
 function App() {
   return (
     <div>
+      <Layout>
+        <p>Test</p>
+      </Layout>
     </div>
   );
 }
