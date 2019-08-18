e3e288a3d937c19a5e9d7bc1adc27ecc423dcc6b
Lưu Minh Hoàng <ceriagame@gmail.com>
2019-08-18 15:52:19 +0700

    Reconstructed containers and components

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 import Aux from '../../hoc/Aux';
 
-import Burger from './Burger/Burger';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
   state = {
