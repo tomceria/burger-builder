cd028bf25edec14d0bfac546f75dafd81c14230b
Lưu Minh Hoàng <ceriagame@gmail.com>
2019-08-18 17:11:51 +0700

    8_161 Removing Ingredients Safely

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,11 +20,11 @@ class BurgerBuilder extends Component {
       cheese: 0,
       meat: 0,
     },
-    totalPrice: 4, // base price
+    totalPrice: 4,
   };
 
   addIngredientHandler = type => {
-    const updatedIngredients = {  // immutable
+    const updatedIngredients = {
       ...this.state.ingredients,
     };
     updatedIngredients[type] = this.state.ingredients[type] + 1;
@@ -35,15 +35,33 @@ class BurgerBuilder extends Component {
   };
 
   removeIngredientHandler = type => {
-    
+    const updatedIngredients = {
+      ...this.state.ingredients,
+    };
+    if (updatedIngredients[type] <= 0) {
+      return;
+    }
+    updatedIngredients[type] = this.state.ingredients[type] - 1;
+    this.setState({ingredients: updatedIngredients});
+    this.setState((prevState, props) => {
+      return {totalPrice: prevState.totalPrice - INGREDIENT_PRICES[type]};
+    });
   };
 
   render() {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+    for (let i in disabledInfo) {
+      disabledInfo[i] = (disabledInfo[i]<=0);  // boolean
+    }
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
           ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+          disabled={disabledInfo}
         />
       </Aux>
     );
