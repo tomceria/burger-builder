5950fe856cd78bc96915982bba42dee54823b6b3
Lưu Minh Hoàng <ceriagame@gmail.com>
2019-08-19 16:14:47 +0700

    8_172 Reusable Nav Items

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,6 +3,7 @@ import React from 'react';
 import styles from './Toolbar.module.css';
 
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => {
   return (
@@ -10,7 +11,7 @@ const toolbar = (props) => {
       <div>MENU</div>
       <Logo />
       <nav>
-        ...
+        <NavigationItems />
       </nav>
     </header>
   );
